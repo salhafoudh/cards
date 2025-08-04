3be90896637bb1b96d1640e9a3389118b2ef80ac
Silvia Al Hafoudh <silvia.alhafoudh@magnepelia.com>
2025-08-04 08:18:48 +0200

    Add styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get "cards", to: "cards#index"
-  get "cards/new", to: "cards#new"
+  get "cards/new", to: "cards#new", as: :new_card
   post "cards", to: "cards#create"
   get "cards/:id", to: "cards#show", as: :card
   patch "cards/:id", to: "cards#update"
   delete "cards/:id", to: "cards#destroy", as: :delete_card
   get "cards/:id/edit", to: "cards#edit", as: :edit_card
+
+  root to: "cards#index"
 end
